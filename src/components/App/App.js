e46ea7955883182772c82e17505db6aa78e72355
Andrew Whitley <awhitley1233@gmail.com>
2017-09-06 09:23:38 -0400

    update starter code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,61 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 import Search from '../Search/Search.js'
 import './App.css'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      searchPhrase: null,
-      langOptions: [
-        { short: "en", name: "English" },
-        { short: "es", name: "Spanish" },
-        { short: "de", name: "German" },
-        { short: "fr", name: "French" },
-        { short: "it", name: "Italian" },
-        { short: "ja", name: "Japanese" },
-        { short: "pt", name: "Portugeuse" },
-      ],
-      sourceLang: null,
-      targetLang: null,
-      translation: null
-    }
-  }
-
-  handleSearchInput(e) {
-    this.setState({
-      searchPhrase: e.target.value
-    })
-  }
-
-  setSourceLang(e) {
-    this.setState({
-      sourceLang: e.target.value
-    })
-  }
-
-  setTargetLang(e) {
-    this.setState({
-      targetLang: e.target.value
-    })
-  }
-
-  handleSearchSubmit(e) {
-    e.preventDefault()
-    axios.get('https://watson-api-explorer.mybluemix.net/language-translator/api/v2/translate', {
-      params: {
-        source: this.state.sourceLang,
-        target: this.state.targetLang,
-        text: this.state.searchPhrase
-      }
-    }).then((response) => {
-      this.setState({
-        translation: response.data.translations[0].translation
-      })
-    })
-  }
-
   render() {
     return(
       <div>
@@ -63,14 +10,7 @@ class App extends Component {
           <h1>React Translator</h1>
         </nav>
         <main>
-          <Search
-            onSearchInput={(e) => this.handleSearchInput(e)}
-            langOptions={this.state.langOptions}
-            setSourceLang={(e) => this.setSourceLang(e)}
-            setTargetLang={(e) => this.setTargetLang(e)}
-            onSearchSubmit={(e) => this.handleSearchSubmit(e)}
-            translation={this.state.translation}
-          />
+          <Search />
         </main>
       </div>
     )
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './Search.css'
 
 class Search extends Component {
+  constructor() {
+    super()
+    this.state = {
+      searchPhrase: null,
+      langOptions: [
+        { short: "en", name: "English" },
+        { short: "es", name: "Spanish" },
+        { short: "de", name: "German" },
+        { short: "fr", name: "French" },
+        { short: "it", name: "Italian" },
+        { short: "ja", name: "Japanese" },
+        { short: "pt", name: "Portugeuse" },
+      ],
+      sourceLang: null,
+      targetLang: null,
+      translation: null
+    }
+  }
+
+  handleSearchInput(e) {
+    this.setState({
+      searchPhrase: e.target.value
+    })
+  }
+
+  setSourceLang(e) {
+    this.setState({
+      sourceLang: e.target.value
+    })
+  }
+
+  setTargetLang(e) {
+    this.setState({
+      targetLang: e.target.value
+    })
+  }
+
+  handleSearchSubmit(e) {
+    e.preventDefault()
+    axios.get('https://watson-api-explorer.mybluemix.net/language-translator/api/v2/translate', {
+      params: {
+        source: this.state.sourceLang,
+        target: this.state.targetLang,
+        text: this.state.searchPhrase
+      }
+    })
+    .then((response) => {
+      this.setState({
+        translation: response.data.translations[0].translation
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
   render() {
-    let langOptions = this.props.langOptions.map((language, index) => {
+    let langOptions = this.state.langOptions.map((language, index) => {
       return(
         <option key={index + 1} value={language.short}>{language.name}</option>
       )
@@ -13,27 +70,27 @@ class Search extends Component {
     )
     return(
       <div>
-        <form onSubmit={(e) => this.props.onSearchSubmit(e)}>
+        <form onSubmit={(e) => this.handleSearchSubmit(e)}>
           <p>Please enter text to be translated and the source and target languages</p>
           <p>
             <label>Text:</label>
-            <textarea onChange={(e) => this.props.onSearchInput(e)}></textarea>
+            <textarea onChange={(e) => this.handleSearchInput(e)}></textarea>
           </p>
           <p>
             <label>From: </label>
-            <select onChange={(e) => this.props.setSourceLang(e)}>
+            <select onChange={(e) => this.setSourceLang(e)}>
               {langOptions}
             </select>
           </p>
           <p>
             <label>To: </label>
-            <select onChange={(e) => this.props.setTargetLang(e)}>
+            <select onChange={(e) => this.setTargetLang(e)}>
               {langOptions}
             </select>
           </p>
           <input type="submit" value="Translate"/>
         </form>
-        <h2>{this.props.translation}</h2>
+        <h2>{this.state.translation}</h2>
       </div>
     )
   }
